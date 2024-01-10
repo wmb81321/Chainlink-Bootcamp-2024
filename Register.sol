9f5459097700c4d98c17a4fab2089f9ff8af7596
William Martinez <68308482+wmb81321@users.noreply.github.com>
2024-01-09 19:52:42 -0500

    Update Register.sol

--- Register.sol
+++ Register.sol
@@ -8,7 +8,7 @@ contract Register {
     	return info;
 	}
 
-
+// es una funcion que permite adquirir informacion publica. Utiliza entradas de tipo String que no tiene restricciones
 
 	function setInfo(string memory _info) public {
     	info = _info;
