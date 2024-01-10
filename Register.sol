c6451f5090ed19a3bab38166eeeff10b690e6e68
William Martinez <68308482+wmb81321@users.noreply.github.com>
2024-01-09 19:48:27 -0500

    Create Register.sol

---
+++ Register.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+contract Register {
+	string private info;
+    
+	function getInfo() public view returns (string memory) {
+    	return info;
+	}
+
+
+
+	function setInfo(string memory _info) public {
+    	info = _info;
+	}
+}
